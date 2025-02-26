79c0b6ef40d16f344058881f22ad63b9837794bf
Maxime Junca-Quintero <maximequintero@gmail.com>
2025-02-26 13:04:01 +0100

    V.1.2.0

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
 from app import app, db
+from scripts.seed_db import seed_database
 
 # Initialisation de la base de données au démarrage
 with app.app_context():
     db.create_all()
+    seed_database()
     print("Base de données initialisée !")
 
 if __name__ == "__main__":
