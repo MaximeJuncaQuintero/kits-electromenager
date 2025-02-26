8c0aae1c981204c1b055d69172819e3de50ff140
Maxime Junca-Quintero <maximequintero@gmail.com>
2025-02-26 12:07:26 +0100

    V1.1.7

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,12 @@
-from app import app
+from app import app, db
+
+# Initialisation de la base de données au démarrage
+with app.app_context():
+    db.create_all()
+    print("Base de données initialisée !")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001)
 else:
     # Pour Render
-    application = app 
+    application = app
